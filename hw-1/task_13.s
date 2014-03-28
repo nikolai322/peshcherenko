6662d5d5d63b1e44295a6fb22919fc44600fe6c1
nikolai322 <peshcherenko146@yandex.ru>
2014-03-28 06:00:55 +0300

    Update task_13.s

--- hw-1/task_13.s
+++ hw-1/task_13.s
@@ -10,28 +10,28 @@ format_string_OF:
 .text
 label_0_ZF:
 	pushl $0
-	jmp label_print_ZF
+	jmp label_continue_ZF
 label_1_ZF:
 	pushl $1
-	jmp label_print_ZF
+	jmp label_continue_ZF
 label_0_SF:
 	pushl $0
-	jmp label_print_SF
+	jmp label_continue_SF
 label_1_SF:
 	pushl $1
-	jmp label_print_SF
+	jmp label_continue_SF
 label_0_OF:
 	pushl $0
-	jmp label_print_OF
+	jmp label_continue_OF
 label_1_OF:
 	pushl $1
-	jmp label_print_OF
+	jmp label_continue_OF
 label_0_CF:
 	pushl $0
-	jmp label_print_CF
+	jmp label_continue_CF
 label_1_CF:
 	pushl $1
-	jmp label_print_CF
+	jmp label_continue_CF
 .globl main
 main:
 //prolog
@@ -45,31 +45,32 @@ main:
 		
 	je label_0_ZF
 	jne label_1_ZF
-label_print_ZF:
+label_continue_ZF:
+	jb label_0_CF
+	jae label_1_CF
+label_continue_CF:
+	js label_0_SF
+	jns label_1_SF
+label_continue_SF:
+	jno label_0_OF
+	jo label_1_OF
+label_continue_OF:
+	pushl $format_string_OF
+	call printf
+	addl $8, %esp
+	
+	pushl $format_string_SF
+	call printf
+	addl $8, %esp
+	
+	pushl $format_string_CF
+	call printf
+	addl $8, %esp
+	
 	pushl $format_string_ZF
 	call printf
 	addl $8, %esp	
 
-	jb label_0_CF
-	jae label_1_CF
-label_print_CF:
-	pushl $format_string_CF
-	call printf
-	addl $8, %esp
-
-	js label_0_SF
-	jns label_1_SF
-label_print_SF:
-	pushl $format_string_SF
-	call printf
-	addl $8, %esp
-
-	jno label_0_OF
-	jo label_1_OF
-label_print_OF:
-	pushl $format_string_OF
-	call printf
-	addl $8, %esp
 	popl %edx
 	popl %ecx
 	popl %ebx
