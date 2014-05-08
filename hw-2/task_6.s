0694e64c68916642f3fa393ef36f28317c622c2e
nikolai322 <peshcherenko146@yandex.ru>
2014-05-08 22:42:26 +0400

    Create task_6.s

---
+++ hw-2/task_6.s
@@ -0,0 +1,86 @@
+.data
+count:
+	.space 4
+fmt_string:
+	.string "%d"
+array:
+	.space 16
+a:
+	.space 4
+.text
+label_0_CF:
+	movl $0, (%ebx)
+	addl $4, %ebx
+	jmp continue_1
+label_1_CF:
+	movl $1, (%ebx)
+	addl $4, %ebx
+	jmp continue_1
+label_0_ZF:
+	movl $0, (%ebx)
+	addl $4, %ebx
+	jmp continue_2
+label_1_ZF:
+	movl $1, (%ebx)
+	addl $4, %ebx
+	jmp continue_2
+label_0_SF:
+	movl $0, (%ebx)
+	addl $4, %ebx
+	jmp continue_3
+label_1_SF:
+	movl $1, (%ebx)
+	addl $4, %ebx
+	jmp continue_3
+label_0_OF:
+	movl $0, (%ebx)
+	jmp continue_4
+label_1_OF:
+	movl $1, (%ebx)
+	jmp continue_4
+.globl main
+.macro outf
+#Carry flag
+	jc label_1_CF
+	jnc label_0_CF
+continue_1:
+#Zero flag
+	je label_0_ZF
+	jne label_1_ZF
+continue_2:
+#Sign flag
+	js label_1_SF
+	jns label_0_SF
+continue_3:
+#Overflow flag
+	jo label_1_OF
+	jno label_0_OF
+continue_4:
+.endm
+.globl main
+main:
+#prolog
+	pushl %ebp
+	movl %esp, %ebp
+#Main program
+	movl $4, count
+	movl $array, %ebx
+#Now %ebx becomes the pointer to the current array element
+	outf
+	movl $array, %ebx
+start:
+	movl (%ebx), %eax
+	movl %ebx, a
+	pushl %eax
+	pushl $fmt_string
+	call printf
+	movl a, %ebx
+	addl $4, %ebx
+	decl count
+	cmpl $0, count
+	jg start
+#epilog
+  movl $0, %eax
+	movl %ebp, %esp
+	popl %ebp
+	ret
