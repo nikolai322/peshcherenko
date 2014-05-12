51441fd39e3125725a50e5c91e62796280b13a13
nikolai322 <peshcherenko146@yandex.ru>
2014-05-12 19:48:57 +0400

    Create task_2.s

---
+++ ex-2/task_2.s
@@ -0,0 +1,47 @@
+.data
+x:
+	.space 4
+y:
+	.space 4
+z:
+	.space 4
+t:
+	.space 4
+fmt_float:
+	.string "%f"
+.text
+.globl main
+main:
+#prolog
+	pushl %ebp
+	movl %esp, %ebp
+#Main program
+	pushl $x
+	pushl $fmt_float
+	call scanf
+	pushl $y
+	pushl $fmt_float
+	call scanf
+	flds x
+	flds y
+	faddp
+	fstps z# z = x + y;
+	fld1
+	flds z
+	fyl2x
+	fstps t # t = log2(x+y)
+	fldl2t
+	flds t
+	fdivp %st, %st(1)
+	flds t
+	faddp
+#print
+	movl $fmt_float, %eax
+	fstpl 4(%esp)
+	movl %eax, (%esp)
+	call printf
+#epilog
+	movl $0, %eax
+	movl %ebp, %esp
+	popl %ebp
+	ret
